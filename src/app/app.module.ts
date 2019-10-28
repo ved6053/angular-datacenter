92a634af29dc57430b29834a7cb62ad77b9b5968
ved6053 <ved.csse@gmail.com>
2019-10-28 15:10:22 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { AlertModule,ProgressbarModule } from 'ngx-bootstrap';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MetricComponent } from './metric/metric.component';
+import { NodesComponent } from './nodes/nodes.component';
+import { NodesRowComponent } from './nodes-row/nodes-row.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    MetricComponent,
+    NodesComponent,
+    NodesRowComponent
   ],
   imports: [
     BrowserModule,
+     AlertModule.forRoot(),
+     ProgressbarModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/metric/metric.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.css']
+})
+export class MetricComponent  {
+
+	private _value: number = 0;
+	private _max: number = 100;
+
+	@Input('used') 
+	set value(value:number){
+	if(isNaN(value)) value=0;
+	this._value=value;
+	}
+	get value(): number{ return this._value;}
+	@Input('available')
+	set max(max:number){
+		if(isNaN(max)) max=100;
+		this._max=max;
+	} 
+	get max(): number{ return this._max;}
+ 	
+ 	isDanger() {
+		return this.value / this.max > 0.7;
+ 	}
+
+}
