45f02fd0eff9e50262b5ce2438e4613d6905a86d
ved6053 <ved.csse@gmail.com>
2019-10-28 16:54:31 +0530

    view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'datacenter';
+  @ViewChild(DashboardComponent) dashboard: DashboardComponent;
+
+ refresh() {
+	this.dashboard.generateData();
+ }
 }
